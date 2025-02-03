90e201d2df84665cb238ceb45920c6433d2e6443
JohnDev-90 <john90.dev@gmail.com>
2025-02-03 14:27:49 -0800

    Change cors

--- index.js
+++ index.js
@@ -26,16 +26,16 @@ import { resetPasswordRoute } from "./Routes/resetPassword/resetPassword.js";
 
 const app = express();
 
-app.use(cors())
 
-// app.use(
-//   cors({
-//     origin: "http://localhost:5173",
-//     credentials: true,
-//   }),
-// );
 
-app.use(json());
+app.use(
+  cors({
+    origin:"http://localhost:5173",
+    credentials: true,
+  }),
+);
+
+
 
 app.use(cookieParser());
 
@@ -55,11 +55,11 @@ app.use("/api",middleWare,transactionsRoute)
 
 
 export const db = mysql.createConnection({
-  host: process.env.DB_HOST,
+  host: process.env.DB_HOST || "localhost",
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
-  PORT: Number(process.env.DB_PORT) 
+  // PORT: Number(process.env.DB_PORT) 
  
 });
 
