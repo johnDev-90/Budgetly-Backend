4d4db6cfd3f59392155038bd29c45c7e4e67c4de
JohnDev-90 <john90.dev@gmail.com>
2025-02-05 16:04:21 -0800

    change variable

--- index.js
+++ index.js
@@ -62,6 +62,7 @@ export const db = mysql.createConnection({
   user:process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
+  PORT:process.env.DB_PORT || 3306
   
 });
 
